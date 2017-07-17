7be0a6556ed9388d8ed3e57b34a6758203b56fb6
arjaynguyen <arjaynguyen123@hotmail.com>
2017-07-17 09:55:46 -0700

    implement ko

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,9 +1,12 @@
 var yelp_ID = keys.CLIENT_ID;
 var yelp_KEY = keys.CLIENT_SECRET;
 
-function toggleNavbar(){
-    $("#navbar").toggle("slide", 300);
+function ViewModel(){
+    var self = this;
+
+    self.toggleNavbar = function(element, e2){
+        $("#navbar").toggle("slide", 300);
+    }
 };
 
-
-
+ko.applyBindings(new ViewModel());
