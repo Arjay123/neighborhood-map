8fabfa5cfce3d8563558a6b13ae74638465442ca
arjaynguyen <arjaynguyen123@hotmail.com>
2017-07-17 22:04:48 -0700

    populate categories sidebar w/ yelp categories

--- dev/app.py
+++ dev/app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 from flask import Flask
 from flask import render_template
@@ -6,17 +7,54 @@ from flask import url_for
 
 app = Flask(__name__)
 
-YELP_CLIENT_ID = json.loads(
-    open("yelp_auth.json", "r").read())["CLIENT_ID"]
-YELP_CLIENT_SECRET = json.loads(
-    open("yelp_auth.json", "r").read())["CLIENT_SECRET"]
+YELP_CLIENT_ID = json.loads(open("yelp_auth.json", "r")
+    .read())["CLIENT_ID"]
+YELP_CLIENT_SECRET = json.loads(open("yelp_auth.json", "r")
+    .read())["CLIENT_SECRET"]
+YELP_API_KEY = json.loads(open("yelp_auth.json", "r")
+    .read())["API_KEY"]
 
 
 @app.route("/")
 def index():
-    print (YELP_CLIENT_ID)
-    print (YELP_CLIENT_SECRET)
     return render_template("index.html")
+
+
+@app.route("/yelp")
+def yelp_api():
+    endpoint = "https://api.yelp.com/v3/businesses/search"
+    query = "location=San Jose"
+
+    r = requests.get(endpoint + "?" + query, 
+        headers={'Authorization': 'bearer %s' % YELP_API_KEY})
+
+    return r.text
+
+
+@app.route("/categories")
+def get_yelp_food_categories():
+    return open("yelp_categories.json").read()
+    # categories = {}
+    # cat_json = json.loads(open("yelp_categories.json").read())
+
+    # for item in cat_json:
+    #     if "food" in item["parents"]:
+    #         add = True
+
+    #         if "country_whitelist" in item:
+    #             if "US" not in item["country_whitelist"]:
+    #                 add = False
+
+    #         if "country_blacklist" in item:
+    #             if "US" in item["country_blacklist"]:
+    #                 add = False
+
+    #         if add:
+    #             categories[item["title"]] = item["alias"]
+
+    # return categories
+
+
 
 
 if __name__ == "__main__":
--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -1,13 +1,52 @@
-function ViewModel(){
+function ViewModel(categories){
     var self = this;
+    self.categories = categories;
+    // console.log(Object.keys(self.categories));
+    self.category_list = ko.observableArray(Object.keys(self.categories));
+
+    console.log(self.category_list());
 
     self.toggleNavbar = function(element, e2){
         $("#navbar").toggle("slide", 300);
+        console.log(self.test)
     }
 };
 
-ko.applyBindings(new ViewModel());
+// ko.applyBindings(new ViewModel());
 
+$.ajax("/yelp", {
+    success: function(data, status){
+        console.log(data);
+        console.log(status);
+    }
+});
+
+$.ajax("/categories", {
+    success: function(data, status){
+        cats = $.parseJSON(data);
+        categories = {};
+
+        for(key in cats){
+            var item = cats[key];
+            if ($.inArray("food", item["parents"]) != -1) {
+                var add = true
+
+                if ("country_whitelist" in item) 
+                    if ($.inArray("US", item["country_whitelist"]) == -1)
+                        add = false;
+
+                if ("country_blacklist" in item)
+                    if ($.inArray("US", item["country_blacklist"]) != -1)
+                        add = false;
+
+                if (add)
+                    categories[item["title"]] = item["alias"];
+            }
+        }
+        // console.log(categories);
+        ko.applyBindings(new ViewModel(categories));
+    }
+});
 
 // $.ajax("https://api.yelp.com/v3/businesses/search?location=San%20Jose", {
 //     data: {
