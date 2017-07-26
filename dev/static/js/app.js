37feeb25edb5faffe1367be91d07c5246d2ebaf1
arjaynguyen <arjaynguyen123@hotmail.com>
2017-07-26 12:13:38 -0700

    display infowindow when clicked

--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -175,7 +175,7 @@ function ViewModel(categories){
     };
 
     self.restaurant_clicked = function(element){
-        console.log(element);
+        show_restaurant(element);
     };
 
 
--- dev/static/js/map.js
+++ dev/static/js/map.js
@@ -1,4 +1,7 @@
 var map;
+var info_window;
+var active_marker;
+
 var markers = {};
 
 function initMap() {
@@ -7,6 +10,7 @@ function initMap() {
         zoom: 11,
         center: {lat: 37.3382, lng: -121.8863},
     });
+    info_window = new google.maps.InfoWindow();
 
 }
 
@@ -23,12 +27,75 @@ function add_markers(restaurants){
             map: map,
             animation: google.maps.Animation.DROP
         });
-        markers[restaurant.id] = (new_marker);
+
+        
+
+        
+
+        new_marker.addListener("click", function(){
+            show_restaurant(restaurant);
+        });
+
+        new_marker.addListener("mouseover", function(){
+            new_marker.setAnimation(google.maps.Animation.BOUNCE);
+        });
+
+        new_marker.addListener("mouseout", function(){
+            new_marker.setAnimation(null);
+        });
+
+        markers[restaurant.id] = new_marker;
         bounds.extend(new_marker.position);
     });
 
     map.fitBounds(bounds);
 };
+
+
+function change_active_marker(id){
+    reset_active_marker();
+    active_marker = id;
+};
+
+function reset_active_marker(){
+    if(active_marker){
+        var marker = markers[active_marker];
+        marker.setIcon();    
+    }
+    
+};
+
+function show_restaurant(restaurant){
+    change_active_marker(restaurant.id);
+    open_window(restaurant);
+    change_color(restaurant.id);
+};
+
+
+function open_window(restaurant){
+
+    var marker = markers[restaurant.id];
+    var content = "<div><div class='info-window-hdr'>" + 
+                    "<h3>" + restaurant.name + "</h3>" +
+                  "</div>" + 
+                  "<div class='restaurant-details'>" + 
+                    "<div data-bind='template: { name: rating }'></div>" +
+                  "</div></div>";
+    
+    info_window.close();
+    info_window.setContent(content);
+    info_window.open(map, marker);
+    marker.setIcon();
+
+
+};
+
+function change_color(restaurant_id){
+    var marker = markers[restaurant_id];
+    marker.setIcon("http://maps.google.com/mapfiles/ms/icons/yellow-dot.png");
+}
+
+
 
 function clear_markers(){
     for(var key in markers){
