f3917599949741c529678658d239ca09db7c8a07
arjaynguyen <arjaynguyen123@hotmail.com>
2017-08-01 07:43:29 -0700

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
-
 var inject = require('gulp-inject');
-var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var htmlreplace = require('gulp-html-replace');
@@ -12,16 +10,18 @@ gulp.task('html', function(){
         .pipe(gulp.dest('prod/templates'));
 });
 
-gulp.task('python', function(){
-    return gulp.src(['dev/app.py', 'dev/yelp_auth.json'])
-        .pipe(gulp.dest('prod/'))
-})
+gulp.task('json', function(){
+    return gulp.src('dev/*.json')
+        .pipe(gulp.dest('prod/'));
+});
 
+gulp.task('python', function(){
+    return gulp.src('dev/app.py')
+        .pipe(gulp.dest('prod/'))
+});
 
 gulp.task('scripts', function(){
-    return gulp.src(['!dev/static/js/keys.js', 'dev/static/js/*.js'])
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
+    return gulp.src('dev/static/js/*.js')
         .pipe(gulp.dest('prod/static/js'));
 });
 
@@ -32,21 +32,18 @@ gulp.task('styles', function(){
         .pipe(gulp.dest('prod/static/css'));
 });
 
-
-// gulp.task('buildIndex', ['scripts', 'html', 'styles'], function(){
-//     return gulp.src('./prod/index.html')
-//         .pipe(inject(gulp.src(['./prod/js/*.min.js', './prod/css/*.min.css'], {read: false}), {relative: true}))
-//         .pipe(gulp.dest('./prod'));
-// });
+gulp.task('imgs', function(){
+    return gulp.src('dev/static/img/*.png')
+        .pipe(gulp.dest('prod/static/img/'));
+});
 
 gulp.task('htmlreplace', ['scripts', 'html'], function() {
-  gulp.src('prod/templates/index.html')
-    .pipe(htmlreplace({
-        'css': '<link rel="stylesheet" href="{{url_for(\'static\', filename=\'css/style.min.css\')}}">',
-        'js': '<script src="{{url_for(\'static\', filename=\'js/app.min.js\')}}"></script>'
-    }))
-    .pipe(gulp.dest('prod/templates/'));
+    gulp.src('prod/templates/index.html')
+        .pipe(htmlreplace({
+            'css': '<link rel="stylesheet" href="{{url_for(\'static\', filename=\'css/style.min.css\')}}">'
+        }))
+        .pipe(gulp.dest('prod/templates/'));
 });
 
 
-gulp.task('default', ['htmlreplace', 'html', 'python', 'scripts', 'styles']);
+gulp.task('default', ['htmlreplace', 'html', 'python', 'styles', 'json', 'scripts', 'imgs']);
